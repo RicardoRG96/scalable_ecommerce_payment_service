429a327b588564384e43a335130ac9e060e5e49f
RicardoRG96 <ricardoretamal10@gmail.com>
2025-04-14 05:34:50 -0400

    add PaymentDetail Entity

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/payment_service/entities/PaymentDetail.java
@@ -0,0 +1,170 @@
+package com.ricardo.scalable.ecommerce.platform.payment_service.entities;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.ricardo.scalable.ecommerce.platform.libs_common.entities.Order;
+import com.ricardo.scalable.ecommerce.platform.libs_common.enums.PaymentStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "payment_details")
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @NotNull
+    private Order order;
+
+    @NotNull
+    private Integer amount;
+
+    @NotBlank
+    private String currency;
+
+    @NotBlank
+    private String provider;
+
+    @Column(name = "payment_method")
+    @NotBlank
+    private String paymentMethod;
+
+    @Column(name = "transaction_id")
+    @NotNull
+    private Long transactionId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private PaymentStatus status;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @CreationTimestamp
+    private Timestamp  updatedAt;
+
+    public PaymentDetail() {
+    }
+
+    public PaymentDetail(Long id, @NotNull Order order, @NotNull Integer amount, @NotBlank String currency,
+            @NotBlank String provider, @NotBlank String paymentMethod, @NotNull Long transactionId,
+            @NotNull PaymentStatus status, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.order = order;
+        this.amount = amount;
+        this.currency = currency;
+        this.provider = provider;
+        this.paymentMethod = paymentMethod;
+        this.transactionId = transactionId;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentDetail [id=" + id + ", order=" + order + ", amount=" + amount + ", currency=" + currency
+                + ", provider=" + provider + ", paymentMethod=" + paymentMethod + ", transactionId=" + transactionId
+                + ", status=" + status + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+    }
+
+}
