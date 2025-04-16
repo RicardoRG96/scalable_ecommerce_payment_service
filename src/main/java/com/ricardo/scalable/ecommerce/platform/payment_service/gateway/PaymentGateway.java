fd357cc19172358e01c4f4813a467f7ab1e8f7c6
RicardoRG96 <ricardoretamal10@gmail.com>
2025-04-16 05:17:46 -0400

    add PaymentGateway interface

--- src/main/java/com/ricardo/scalable/ecommerce/platform/payment_service/gateway/PaymentGateway.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/payment_service/gateway/PaymentGateway.java
@@ -1,5 +1,10 @@
 package com.ricardo.scalable.ecommerce.platform.payment_service.gateway;
 
+import com.ricardo.scalable.ecommerce.platform.payment_service.repositories.dto.PaymentRequest;
+import com.ricardo.scalable.ecommerce.platform.payment_service.repositories.dto.PaymentResponse;
+
 public interface PaymentGateway {
+
+    PaymentResponse processPayment(PaymentRequest request);
 
 }
