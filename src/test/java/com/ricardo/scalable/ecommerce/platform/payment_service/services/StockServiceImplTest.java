dfb091caf2974e0dbf339124b86d3dfd6a91bd03
RicardoRG96 <ricardoretamal10@gmail.com>
2025-05-10 13:19:35 -0400

    add test to updateStockAfterPayment()

--- src/test/java/com/ricardo/scalable/ecommerce/platform/payment_service/services/StockServiceImplTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/payment_service/services/StockServiceImplTest.java
@@ -4,23 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static com.ricardo.scalable.ecommerce.platform.payment_service.services.testData.utils.OrderTestData.*;
 import static com.ricardo.scalable.ecommerce.platform.payment_service.services.testData.utils.ProductSkuTestData.*;
 import static com.ricardo.scalable.ecommerce.platform.payment_service.services.testData.utils.OrderItemTestData.*;
+import static com.ricardo.scalable.ecommerce.platform.payment_service.services.testData.PaymentDetailServiceImplTestData.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import java.lang.foreign.Linker.Option;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 import com.ricardo.scalable.ecommerce.platform.libs_common.entities.OrderItem;
-import com.ricardo.scalable.ecommerce.platform.libs_common.entities.ProductSku;
 import com.ricardo.scalable.ecommerce.platform.libs_common.exceptions.InsufficientStockException;
 import com.ricardo.scalable.ecommerce.platform.libs_common.exceptions.ResourceNotFoundException;
+import com.ricardo.scalable.ecommerce.platform.payment_service.exception.PaymentNotConfirmedException;
 import com.ricardo.scalable.ecommerce.platform.payment_service.model.repository.OrderItemRepository;
 import com.ricardo.scalable.ecommerce.platform.payment_service.model.repository.PaymentDetailRepository;
 import com.ricardo.scalable.ecommerce.platform.payment_service.model.repository.ProductSkuRepository;
@@ -70,6 +68,60 @@ public class StockServiceImplTest {
         when(productSkuRepository.findById(2L)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> stockService.verifyStock(1L));
+    }
+
+    @Test
+    void updateStockAfterPayment_whenTransactionIdDoesNotExist_shouldThrowResourceNotFoundException() {
+        String transactionId = "transactionId";
+        when(paymentDetailRepository.findByTransactionId(transactionId)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> stockService.updateStockAfterPayment(transactionId));
+    }
+
+    @Test
+    void updateStockAfterPayment_whenPaymentStatusIsNotCompleted_shouldThrowPaymentNotConfirmedException() {
+        String transactionId = "TXN5566778899";
+
+        when(paymentDetailRepository.findByTransactionId(transactionId)).thenReturn(createPaymentDetail004());
+
+        assertThrows(PaymentNotConfirmedException.class, () -> stockService.updateStockAfterPayment(transactionId));
+    }
+
+    @Test
+    void updateStockAfterPayment_whenOrderItemsAreEmpty_shouldThrowResourceNotFoundException() {
+        String transactionId = "TXN1234567890";
+
+        when(paymentDetailRepository.findByTransactionId(transactionId)).thenReturn(createPaymentDetail001());
+        when(orderItemRepository.findByOrderId(1L)).thenReturn(List.of());
+
+        assertThrows(ResourceNotFoundException.class, () -> stockService.updateStockAfterPayment(transactionId));
+    }
+
+    @Test
+    void updateStockAfterPayment_whenQuantityIsGreaterThanStock_shouldThrowInsufficientStockException() {
+        String transactionId = "TXN1234567890";
+        List<OrderItem> orderItems = createListOfOrderItemByOrderId1().orElseThrow();
+        orderItems.get(0).setQuantity(21);
+
+        when(paymentDetailRepository.findByTransactionId(transactionId)).thenReturn(createPaymentDetail001());
+        when(orderItemRepository.findByOrderId(1L)).thenReturn(orderItems);
+        when(productSkuRepository.findById(1L)).thenReturn(createProductSku001());
+        when(productSkuRepository.findById(2L)).thenReturn(createProductSku002());
+
+        assertThrows(InsufficientStockException.class, () -> stockService.updateStockAfterPayment(transactionId));
+    }
+
+    @Test
+    void updateStockAfterPayment_whenProductSkuNotFound_shouldThrowResourceNotFoundException() {
+        String transactionId = "TXN1234567890";
+        List<OrderItem> orderItems = createListOfOrderItemByOrderId1().orElseThrow();
+
+        when(paymentDetailRepository.findByTransactionId(transactionId)).thenReturn(createPaymentDetail001());
+        when(orderItemRepository.findByOrderId(1L)).thenReturn(orderItems);
+        when(productSkuRepository.findById(1L)).thenReturn(createProductSku001());
+        when(productSkuRepository.findById(2L)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> stockService.updateStockAfterPayment(transactionId));
     }
 
 }
